70c96ae7507f0a5d8fddb6be1352689cf5d336fd
Roy Crihfield <rscrihf@gmail.com>
2015-02-21 15:46:19 -0500

    update for 1.0.0-dev

--- quadtree.rs
+++ quadtree.rs
@@ -1,39 +1,55 @@
 // use std::iter::AdditiveIterator;
 // use std::ops::IndexMut;
 
-type Float = f64;
-pub type Point = (Float, Float);
+// use NodeVariant::{Branch, Bucket};
 
-const MAX_BUCKET_CAPACITY: uint = 16;
-const MAX_NODE_DEPTH: uint = 32;
+const MAX_BUCKET_CAPACITY: usize = 16;
+const MAX_NODE_DEPTH: usize = 32;
+
+pub type Point = (f64, f64);
 
 pub struct Quadtree<T> where T: Sized {
     root: Node<T>,
-    dimensions: (Float, Float),
+    dimensions: (f64, f64),
+}
+
+type Child<T> = Box<Node<T>>;
+
+// #[deriving(Show)]
+enum NodeVariant<T> where T: Sized {
+    Branch([Child<T>; 4]),
+    Bucket(Vec<(Point, T)>)
+}
+
+struct Node<T> {
+    center: Point,
+    variant: NodeVariant<T>
 }
 
 impl<T> Quadtree<T> {
-    pub fn new(center: Point, w: Float, h: Float) -> Quadtree<T> {
+    pub fn new(center: Point, w: f64, h: f64) -> Quadtree<T> {
         Quadtree {
             root: Node {
                 center: center,
-                variant: Bucket(Vec::new())
+                variant: NodeVariant::Bucket(Vec::new())
             },
             dimensions: (w, h)
         }
     }
     
-    pub fn len(&self) -> uint {
-        let mut sum: uint = 0u;
-        self.root.traverse(|node| match node.variant {
-            Bucket(ref data) => sum += data.len(), _ => return
-        });
+    pub fn len(&self) -> usize {
+        let mut sum: usize = 0;
+        self.root.traverse(
+            &mut |node| match node.variant {
+                NodeVariant::Bucket(ref data) => sum += data.len(),
+                _ => return
+            });
         sum
     }
 
-    pub fn node_count(&self) -> uint {
-        let mut sum: uint = 0u;
-        self.root.traverse(|_| sum += 1);
+    pub fn node_count(&self) -> usize {
+        let mut sum: usize = 0;
+        self.root.traverse(&mut |_| sum += 1);
         sum
     }
     
@@ -43,8 +59,8 @@ impl<T> Quadtree<T> {
             Some((node, depth_sub)) => {
                 let (w, h) = self.dimensions;
                 let depth = MAX_NODE_DEPTH - depth_sub + 1;
-                // let denom: Float = ::std::num::pow(2., depth);
-                let denom = (2u << depth) as Float;
+                // let denom: f64 = ::std::num::pow(2., depth);
+                let denom = (2 << depth) as f64;
                 unsafe {
                     (*node).split(w / denom, h / denom);
                 }
@@ -54,20 +70,7 @@ impl<T> Quadtree<T> {
     }
 }
 
-type Child<T> = Box<Node<T>>;
-
-// #[deriving(Show)]
-enum NodeVariant<T> where T: Sized {
-    Branch([Child<T>, ..4]),
-    Bucket(Vec<(Point, T)>)
-}
-
-struct Node<T> {
-    center: Point,
-    variant: NodeVariant<T>
-}
-
-fn quadrant((x0, y0): Point, (x, y): Point) -> uint {
+fn quadrant((x0, y0): Point, (x, y): Point) -> usize {
     match (x < x0, y < y0) {
         (true, true) => 0,
         (_, true)    => 1,
@@ -78,66 +81,71 @@ fn quadrant((x0, y0): Point, (x, y): Point) -> uint {
 
 impl<T> Node<T> {
 
-    fn traverse(&self, f: |&Node<T>|) {
+    fn traverse(&self, f: &mut FnMut(&Node<T>) -> ()) {
         f(self);
         match self.variant {
-            Branch(ref children) => {
+            NodeVariant::Branch(ref children) => {
                 for child in children.iter() {
-                    child.traverse(|child| f(child));
+                    child.traverse(&mut |child| f(child));
                 }
             },
             _ => return
         }
     }
 
-    fn split(&mut self, dx: Float, dy: Float) {
+    fn split(&mut self, dx: f64, dy: f64) {
         use std::mem::swap;
         
         let (x0, y0) = self.center;
-        let mut children: [Child<T>, ..4] = unsafe {::std::mem::uninitialized()};
+        // let mut children: [Child<T>, ..4] = unsafe {::std::mem::uninitialized()};
+        let mut children: [Child<T>; 4] = unsafe {::std::mem::uninitialized()};
         
         match self.variant {
-            Bucket(ref mut data) => {
+            NodeVariant::Bucket(ref mut data) => {
 
-                let make_center = |i: uint| {
-                    let x = if i % 2 == 0 { x0 - dx } else { x0 + dx };
-                    let y = if i < 2 { y0 - dy } else { y0 + dy };
+                let make_center = |&: quadrant: usize| {
+                    let x = if quadrant % 2 == 0 { x0 - dx } else { x0 + dx };
+                    let y = if quadrant < 2 { y0 - dy } else { y0 + dy };
                     (x, y)
                 };
                 
-                let mut child_data: Vec<Vec<(Point, T)>> = Vec::from_fn(4, |_| Vec::new());
+                // let mut child_data: Vec<Vec<(Point, T)>> = vec![Vec::new(); 4];
+                let mut child_data: Vec<Vec<(Point, T)>> = Vec::new();
+                // child_data.resize(4, Vec::new());
+                // let mut child_data: Vec<Vec<(Point, T)>> = Vec::from_elem(4, Vec::new());
+                for _ in 1..4 { child_data.push(Vec::new()); }
                 let mut data_swap = Vec::new();
                 swap(data, &mut data_swap);
 
                 for (pt, val) in data_swap.into_iter() {
                     let q = quadrant(self.center, pt);
-                    child_data.get_mut(q).push((pt, val));
+                    child_data[q].push((pt, val));
                 }
 
                 for (i, child) in children.iter_mut().enumerate().rev() {
                     // println!("child_data.len = {}, i = {}", child_data.len(), i);
                     
-                    let new = box Node {
+                    let new = Box::new(Node {
                         center: make_center(i),
-                        variant: Bucket(child_data.pop().unwrap())
-                    };
+                        variant: NodeVariant::Bucket(child_data.pop().unwrap())
+                    });
                     unsafe { ::std::ptr::write(child, new); }
                 }
             },
             _ => unreachable!()
         }
-        self.variant = Branch(children);
+        self.variant = NodeVariant::Branch(children);
     }
     
-    fn push(&mut self, depth: uint, (pt, value): (Point, T)) -> Option<(*mut Node<T>, uint)> {
+    fn push(&mut self, depth: usize, (pt, value): (Point, T)) -> Option<(*mut Node<T>, usize)> {
         // println!("push(depth={}, pt={}, _) (Node (center={}))", depth, pt, self.center);
             
         match self.variant {
-            Branch(ref mut children) => {
+            NodeVariant::Branch(ref mut children) => {
                 let q = quadrant(self.center, pt);
                 children[q].push(depth - 1, (pt, value))
             },
-            Bucket(ref mut data) => {
+            NodeVariant::Bucket(ref mut data) => {
                 data.push((pt, value));
                 if data.len() == MAX_BUCKET_CAPACITY && depth != 0 {
                     Some((self as *mut Node<T>, depth))
--- test_qt.rs
+++ test_qt.rs
@@ -1,20 +1,24 @@
-#![feature(macro_rules)]
 #![feature(slicing_syntax)]
-#![feature(phase)]
+// #![feature(phase)]
 
 // #[phase(plugin, link)] extern crate log;
-extern crate time;
+use std::time;
 
 mod quadtree;
 
+// TODO make time work...
+fn now() -> u64 { time::precise_time_ns() }
+
 macro_rules! benchmark(
-    ($what: expr) => { {        
-        let start_time = time::precise_time_ns();
-        let ret = $what;
-        let end_time = time::precise_time_ns();
-        (ret, end_time - start_time)
-    } }
-)
+    ($what: expr) => {
+        {        
+            let start_time = now();
+            let ret = $what;
+            let end_time = now();
+            (ret, end_time - start_time)
+        }
+    }
+);
 
 fn main() {
     use quadtree::{Quadtree, Point};
@@ -22,9 +26,10 @@ fn main() {
     use std::rand::Rng;
 
     let args = std::os::args();
-    let n = match args[] {
-        [_, ref a, ..] => match from_str::<uint>(a[]) {
-            Some(n) => n, _ => {
+    let n = match &*args {
+        [_, ref a, ..] => match a[].parse::<u32>() {
+            Some(n) => n,
+            _ => {
                 println!("enter a positive integer");
                 return
             }
@@ -32,15 +37,15 @@ fn main() {
         _ => 1000
     };
     
-    let mut tree: Quadtree<int> = Quadtree::new((0.0, 0.0), 1.0, 1.0);
+    let mut tree: Quadtree<i32> = Quadtree::new((0.0, 0.0), 1.0, 1.0);
 
-    let mut rng = std::rand::task_rng();
+    let mut rng = std::rand::thread_rng();
     let dist = Range::new(-0.5, 0.5);
-
-    let points = Vec::<(Point, int)>::from_fn(n, |_| {
+    
+    let points = (1..n).map(|_| -> (Point, i32) {
         let p = (dist.ind_sample(&mut rng), dist.ind_sample(&mut rng));
         (p, rng.gen())
-    });
+    }).collect::<Vec<(Point, i32)>>();
 
     let (_, t_insert) = benchmark!(for e in points.into_iter() { tree.push(e); });
     
